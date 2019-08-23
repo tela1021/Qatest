709e451beb80478fd3664160a23c7d5594f1bd1c
telas1021 <telas1020@gmail.com>
2019-08-23 10:08:08 +0300

    Commit

--- src/test/java/test/tetsDomawka.java
+++ src/test/java/test/tetsDomawka.java
@@ -1,4 +1,25 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
 public class tetsDomawka {
+    @Test
+    public void CalcSymb() throws IOException {
+
+        String line = "aasfasasgsahdfhds";
+        char Letter = 'a';
+        int count = 0;
+        for (char element : line.toCharArray()) {
+            if (element == Letter) count++;
+
+        }
+        Assert.assertEquals(4, count);
+    }
 }
